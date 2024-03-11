8bb43d5117e239ea880288daa7c200e3909e57a1
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-03-11 22:58:52 +0800

    Show OperatePanel

--- Tais.Godot/MainScene/LeftPanels/CityDetailPanel/CityDetailPanel.cs
+++ Tais.Godot/MainScene/LeftPanels/CityDetailPanel/CityDetailPanel.cs
@@ -6,4 +6,19 @@ public partial class CityDetailPanel : LeftContentPanel
     public object Id { get; internal set; }
 
     public override Button CloseButton => GetNode<Button>("HBoxContainer/VBoxContainer2/CityName/Close");
+
+    public Button OperateButton => GetNode<Button>("HBoxContainer/VBoxContainer2/CityName/Operate");
+
+    public Control OperatePanel => GetNode<Control>("HBoxContainer/VBoxContainer");
+
+    public override void _Ready()
+    {
+        base._Ready();
+
+        OperatePanel.Visible = OperateButton.ButtonPressed;
+        OperateButton.Toggled += (flag) =>
+        {
+            OperatePanel.Visible = flag;
+        };
+    }
 }
