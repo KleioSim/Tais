39a777080607abeffded5d350fd1551565cd0fee
Simon.S.Fong <Simon.S.Fong@outlook.com>
2024-04-18 19:49:42 +0800

    Show Warning Detail

---
+++ Tais.Godot/MainScene/Presents/WarnItemPresent.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Tais.Interfaces;
+
+public partial class WarnItemPresent : PresentControl<WarnItem, ISession>
+{
+    protected override void Initialize(WarnItem view, ISession model)
+    {
+        var warnObj = view.Id as IWarn;
+        view.TooltipTrigger.funcGetToolTipString = () =>
+        {
+            return string.Join("\n", warnObj.Items);
+        };
+    }
+
+    protected override void Update(WarnItem view, ISession model)
+    {
+        var warnObj = view.Id as IWarn;
+
+        view.CountPanel.Visible = warnObj.Items.Count() > 1;
+        view.Count.Text = warnObj.Items.Count().ToString();
+    }
+}
--- Tais.Godot/MainScene/Warns/WarnItem.cs
+++ Tais.Godot/MainScene/Warns/WarnItem.cs
@@ -4,4 +4,9 @@ using System;
 public partial class WarnItem : ViewControl, IItemView
 {
     public object Id { get; set; }
+
+    public Control CountPanel => GetNode<Control>("Count");
+    public Label Count => CountPanel.GetNode<Label>("Label");
+
+    public TooltipTrigger TooltipTrigger => GetNode<TooltipTrigger>("TooltipTrigger");
 }
